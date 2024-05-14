a93759c501e22f246118ed2d313c5169f40d322a
Nathaniel Bond <nabond251@gmail.com>
2024-05-13 21:31:08 -0400

    Enable simple animation

--- examples/Rearch.Reactor.Example/Components/Body.cs
+++ examples/Rearch.Reactor.Example/Components/Body.cs
@@ -4,14 +4,25 @@ using Rearch.Reactor.Components;
 using static Rearch.Reactor.Example.Capsules.TodoCapsules;
 using Rearch.Reactor.Example.Pages;
 using System;
+using System.Linq;
+using MauiReactor.Animations;
 
 namespace Rearch.Reactor.Example.Components;
 
 partial class Body : CapsuleConsumer
 {
+    private enum SearchState
+    {
+        Idle,
+        Searching,
+        Search,
+        Idling
+    }
+
     public override VisualNode Render(ICapsuleHandle use)
     {
-        var (isSearching, setIsSearching) = use.State(false);
+        var (searchState, setSearchState) = use.State(SearchState.Idle);
+        var (searchHeight, setSearchHeight) = use.State(0d);
 
         var (AddTodo, _, _) = use.Invoke(TodoItemsManagerCapsule);
 
@@ -30,21 +41,62 @@ partial class Body : CapsuleConsumer
                 .OnClicked(toggleCompletionStatus),
 
                 ToolbarItem("Search")
-                .OnClicked(() => setIsSearching(!isSearching)),
+                .OnClicked(() => setSearchState(
+                    searchState == SearchState.Idle ? SearchState.Searching :
+                    searchState == SearchState.Searching ? SearchState.Idling :
+                    searchState == SearchState.Search ? SearchState.Idling :
+                    SearchState.Searching)),
 
                 ToolbarItem("Create")
                 .OnClicked(() => ShowCreateTodoDialogAsync(
                     ContainerPage, AddTodo)),
 
-                isSearching ?
-                    Grid("Auto, *", "*",
-                        new SearchBar(
-                            close: () => setIsSearching(false)),
+                Grid("Auto, *", "*",
+                    new SearchBar(
+                        height: searchHeight,
+                        close: () => setSearchState(
+                            searchState == SearchState.Idle ? SearchState.Searching :
+                            searchState == SearchState.Searching ? SearchState.Idling :
+                            searchState == SearchState.Search ? SearchState.Idling :
+                            SearchState.Searching)),
 
-                        new TodoList()
-                        .GridRow(1)
-                    ) :
                     new TodoList()
+                    .GridRow(1),
+
+                    new AnimationController
+                    {
+                        new SequenceAnimation
+                        {
+                            new DoubleAnimation()
+                                .StartValue(searchState == SearchState.Idling ? 50 : 0)
+                                .TargetValue(searchState == SearchState.Idling ? 0 : 50)
+                                .Duration(1000)
+                                .OnTick(v =>
+                                {
+                                    setSearchHeight(v);
+                                    switch (searchState)
+                                    {
+                                        case SearchState.Searching:
+                                            if (v == 50)
+                                            {
+                                                setSearchState(SearchState.Search);
+                                            }
+
+                                            break;
+
+                                        case SearchState.Idling:
+                                            if (v == 0)
+                                            {
+                                                setSearchState(SearchState.Idle);
+                                            }
+
+                                            break;
+                                    }
+                                }),
+                        }
+                    }
+                    .IsEnabled(searchState == SearchState.Searching || searchState == SearchState.Idling)
+                )
             )
             .Title("rearch todos")
         );
